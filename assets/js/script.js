8761da699ed1bc8684407d187b7d7205fd0bd8ac
djamiranda <djamiranda@gmail.com>
2023-05-19 00:24:14 -0400

    updated commments on html, css & js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,17 @@ $(document).ready(function() {
   var scheduledEvents = [];
   $("#currentDate").text(dayjs().format("dddd, MMMM D"));
   var currentHour = dayjs().hour();
-
+// get scheduled events, if available
   scheduledEvents = getDaysEvents();
-
+// for loop to display each hour, text area & button.
   for(var i = 0; i < dayHours.length; i++) {
     $("#"+i).append("<section class='col-md-1 col-1' id='scheduleTime'><p>"+dayHours[i]+"</p></section>"+
                     "<form class='col-md-10 col-10' id='formSchedule"+i+"'><section class='form-group'>"+
                     "<textarea class='col-md-10 col-10 description scheduleText' rows='3' id='textSchedule"+i+"'></textarea>"+
                     "</section></form><button class='col-md-1 col-1 saveBtn' id='eventSave"+i+"'>"+
                     "<i class='fa fa-save' data-index='"+i+"' style='font-size:20px'></i></button>");
-    if(i+9 < currentHour) {
+    // Colors each hour as past, present or future.
+        if(i+9 < currentHour) {
             $("#textSchedule"+i).addClass("past 'form-control' required");
         }
         else if(i+9 == currentHour) {
@@ -27,6 +28,7 @@ $(document).ready(function() {
         $("#textSchedule"+i).val(scheduledEvents[i]);
         $("#eventSave"+i).on("click", eventSave);
     }
+    // Get scheduled events from local storage
     function getDaysEvents() {
       var daysEvents = localStorage.getItem("scheduledEvents");
       var tempSchedEvents = ["","","","","","","","",""];
@@ -36,11 +38,13 @@ $(document).ready(function() {
       }
       return tempSchedEvents;
     }
+    // Saves events to storage if entered
       function eventSave(event) {
         var index = event.target.getAttribute("data-index");
         console.log(index)
         var tempSchedEvents = getDaysEvents();
         console.log(tempSchedEvents)
+      //  Updates scheduled event in local storage & displays message confirming
         if ($("#textSchedule"+index).val() !== tempSchedEvents[index]) {
             scheduledEvents.splice(index, 1, $("#textSchedule"+index).val());
             localStorage.setItem("scheduledEvents", JSON.stringify(scheduledEvents));
